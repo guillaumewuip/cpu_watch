03d7f2ebf959cd443e59aea15d963c59d70377a6
Guillaume Wuip <guillaume@wuips.com>
2015-10-07 23:11:22 +0200

    Fix

--- cpu_watch.sh
+++ cpu_watch.sh
@@ -13,12 +13,30 @@ if [ "x$notifier" = "x" ]; then
     notifier=$default_notifier
 fi
 
-prefix=${TMPDIR}cpu_watch
+prefix=/var/tmp/cpu_watch
 mkdir -p $prefix
 
-#echo $cpu_limit
-#echo $processes
-#echo $notifier
+echo $cpu_limit
+echo $processes
+echo $notifier
+
+sendNotif() {
+
+    subtitle=$1
+    cpu=$2
+
+    #remove everything behind ',' (French) or '.'
+    cpu_int=$(echo $cpu | cut -d. -f1 | cut -d, -f1)
+
+
+    #if cpu > cpu_limit
+    if [ $cpu_int -ge $cpu_limit ]; then
+        echo $subtitle $cpu $cpu_int $cpu_limit
+        $notifier -title "$title" -subtitle "$subtitle" -message $cpu \
+            -group $notifier_group -activate $activity -sender $activity \
+            > /dev/null
+    fi
+}
 
 if ((cpu_limit >= 0)) && [ "x$processes" != "x" ]; then
 
@@ -28,19 +46,23 @@ if ((cpu_limit >= 0)) && [ "x$processes" != "x" ]; then
     echo "$current" | while read p; do
         subtitle=$(echo $p | cut -f2- -d\ )
         cpu=$(echo $p | cut -f1 -d\ )\%
+        echo $cpu
 
-        #if file exists and is older than 10min, remove it
-        if [ -e \""$prefix"/"$subtitle"\" ] && test $(find \”"$prefix"/"$subtitle"\" -mmin +"$interval"); then
-            rm $prefix/"$subtitle"
+        #if file exists and is older than 10min, remove it and send nofif
+        if [ -e "$prefix/$subtitle" ] && test $(find "$prefix/$subtitle" -mmin +"$interval"); then
+            echo "file"
+
+            sendNotif "$subtitle" $cpu
+            rm "$prefix/$subtitle"
         fi
 
-        #if cpu > cpu_limit
-        if [ $(echo $cpu | cut -d, -f1) -ge $cpu_limit ]; then
-            $notifier -title "$title" -subtitle "$subtitle" -message $cpu \
-                -group $notifier_group -activate $activity -sender $activity \
-                > /dev/null
+        #if no file
+        if [ ! -e "$prefix/$subtitle" ]; then
+            echo "no file"
+            #create a file in order to not fire a notif every 30sec
+            touch "$prefix/$subtitle"
 
-            touch "$prefix/$subtitle" #create a file in order to not fire a notif every 30sec
+            sendNotif "$subtitle" $cpu
         fi
     done
 fi
